bd3eb88b2605df1f9b0cfda3d53ec119f7d64f38
guangqiang <1126756952@qq.com>
2017-12-12 18:12:22 +0800

    优化Loading组件代码

--- src/utils/progressHUD/progressHUD.js
+++ src/utils/progressHUD/progressHUD.js
@@ -2,10 +2,8 @@
  * Created by guangqiang on 2017/9/10.
  */
 import React, {Component} from 'react'
-import {View, StyleSheet} from 'react-native'
-import * as Progress from 'react-native-progress'
+import {View, StyleSheet, ActivityIndicator} from 'react-native'
 import deviceInfo from '../deviceInfo'
-import {commonStyle} from '../commonStyle'
 import {Actions} from 'react-native-router-flux'
 export default class ProgressHUD extends Component {
 
@@ -19,7 +17,8 @@ export default class ProgressHUD extends Component {
     return (
       <View style={styles.maskStyle}>
         <View style={styles.backViewStyle}>
-          <Progress.Circle size={deviceInfo.deviceWidth / 8} indeterminate={true} />
+          {/*<Progress.Circle size={deviceInfo.deviceWidth / 8} indeterminate={true} />*/}
+          <ActivityIndicator size="large" color="white" />
         </View>
       </View>
     )
@@ -29,19 +28,19 @@ export default class ProgressHUD extends Component {
 const styles = StyleSheet.create({
     maskStyle: {
       position: 'absolute',
-      backgroundColor: 'rgba(0,0,0,0.2)',
+      backgroundColor: 'rgba(0,0,0,0.3)',
       width: deviceInfo.deviceWidth,
       height: deviceInfo.deviceHeight,
       alignItems: 'center',
       justifyContent: 'center'
     },
     backViewStyle: {
-      backgroundColor: commonStyle.white,
-      width: deviceInfo.deviceWidth / 4,
-      height: deviceInfo.deviceWidth / 4,
+      backgroundColor: '#111',
+      width: 120,
+      height: 100,
       justifyContent: 'center',
       alignItems: 'center',
-      borderRadius: 10,
+      borderRadius: 5,
     }
   }
 )
